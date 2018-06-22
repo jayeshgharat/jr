7d5f6cdb26397da5fdd2e0f35120256d4ce9cf74
jayes <jayes@DESKTOP-8VD8TIH>
2018-06-22 10:28:29 +0530

    addition feature added

--- src/life/Hello.java
+++ src/life/Hello.java
@@ -4,7 +4,12 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int a=5,b=6;
 System.out.println("hiiiiii");
+int add=a+ b;
+System.out.println(add);
+
+
 	}
 
 }
